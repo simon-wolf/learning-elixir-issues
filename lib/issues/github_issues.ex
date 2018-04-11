1c435346fba3fb7ca16deed233db960950e0f96a
Simon Wolf <simon@ottersoftware.com>
2018-04-11 14:36:13 +0100

    Added the GitHub fetching code.

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,6 +1,5 @@
 defmodule Issues.GitHubIssues do
-
-  @user_agent [ {"User-agent", "Elixir simon@simonwolf.io"} ]
+  @user_agent [{"User-agent", "Elixir simon@simonwolf.io"}]
 
   def fetch(user, project) do
     issues_url(user, project)
@@ -8,5 +7,15 @@ defmodule Issues.GitHubIssues do
     |> handle_response
   end
 
+  def issues_url(user, project) do
+    "https://api.github.com/repos/#{user}/#{project}/issues"
+  end
 
+  def handle_response({:ok, %{status_code: 200, body: body}}) do
+    {:ok, body}
+  end
+
+  def handle_response({_, %{status_code: _, body: body}}) do
+    {:error, body}
+  end
 end
